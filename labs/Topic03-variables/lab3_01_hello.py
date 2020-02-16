cc1f7f951983f90515378ff0b448dd6363513c48
Daniel Mc Callion <dmceire@gmail.com>
2020-02-16 13:58:24 +0000

    Updated file on variables lesson. Added first lab03 file on outputting a name.

---
+++ labs/Topic03-variables/lab3_01_hello.py
@@ -0,0 +1,6 @@
+# Daniel Mc Callion
+# Program that reads in a persons name asnd use the format function to
+# output hello thatName
+
+name = input("Enter your name: ")
+print("Hello {}".format(name))
--- variables.py
+++ variables.py
@@ -16,3 +16,18 @@ print(z)
 fac = 7 * 6 * 5 * 4 * 3 * 2 * 1
 
 print(fac)
+
+b = True
+c = False
+
+x = True
+
+print(c)
+
+s = "daniel.mccallion"
+
+print(s[0:14:2])
+
+l = [1,2,3,4,5,6,7,8,9]
+
+print(l[2:6:3])
