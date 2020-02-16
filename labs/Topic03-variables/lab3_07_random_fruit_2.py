798a2e9bd21e9bdf4a6f0ff2ad9fd495c976a332
Daniel Mc Callion <dmceire@gmail.com>
2020-02-16 16:17:22 +0000

    Added seventh lab03 file to generate a random fruit using tuples.

---
+++ labs/Topic03-variables/lab3_07_random_fruit_2.py
@@ -0,0 +1,12 @@
+# Daniel Mc Callion
+# Program that prints out a random fruit using tuple and random
+
+import random
+
+fruits = ("Apple", "Banana", "Orange", "Grape", "Pear", "Mango")
+
+# Get a random fruit from tuple
+random_fruit = random.choice(fruits)
+
+# Print output
+print(f"A random fruit: {random_fruit}")
