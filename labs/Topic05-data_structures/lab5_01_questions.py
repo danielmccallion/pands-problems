191d4ba8d273b74a138cb2556b9c1e3ade7328ab
Daniel Mc Callion <dmceire@gmail.com>
2020-02-20 21:57:05 +0000

    Added first lab05 file questions on datatypes

---
+++ labs/Topic05-data_structures/lab5_01_questions.py
@@ -0,0 +1,39 @@
+# Daniel Mc Callion
+
+number_of_questions = 5
+average_age = 23.4
+debug_mode = True
+name = "Joe"
+ages = []
+months = ("Jan", "Feb", "Mar")
+book = {}
+stuff = [12, "Fred", False, {}]
+someone = dict(firstname = "Joe")
+me = {
+    "firstname": "Andrew",
+    "teaching": [{
+        "course_name": "Programming",
+        "semester": 1
+    },{
+        "course_name": "Data Representation",
+        "semester": 2
+    }
+    ]
+}
+
+# a = int
+# b = float
+# c = boolean
+# d = string
+# e = list
+# f = tuple
+# g = string
+# h = dict
+# i = list
+# j = boolean
+# k = dict
+# l = string
+# m = dict
+# n = list
+# o = int
+# p = no type
