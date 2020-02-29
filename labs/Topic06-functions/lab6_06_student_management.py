9d143d09d693c88bb56d0356613b043d79ee3cf7
Daniel Mc Callion <dmceire@gmail.com>
2020-02-29 22:07:34 +0000

    Added sixth lab06 file to merge previous two into student management

---
+++ labs/Topic06-functions/lab6_06_student_management.py
@@ -0,0 +1,59 @@
+# Daniel Mc Callion
+# A program that allows a user to add and view students,
+# their modules and their grades.
+
+# The array to store a students
+students = []
+
+
+# Function to display a menu and return the choice
+def display_menu():
+    print("What would you like to do?")
+    print("\t(a) Add new student")
+    print("\t(v) View students")
+    print("\t(q) Quit")
+    choice = input("Type one letter (a/v/q): ").strip().lower()
+
+    return choice
+
+
+# Future for viewing a student
+def do_view():
+    print(students)
+
+
+# Reads in a students modules and grades from a user
+def read_modules():
+    modules = []
+    module_name = input("\tEnter the first module name (blank to quit): ").strip()
+
+    while module_name != "":
+        module = {}
+        module["name"] = module_name
+        module["grade"] = int(input("\t\tEnter grade: "))
+        modules.append(module)
+
+        module_name = input("\tEnter the next module name (blank to quit): ").strip()
+
+    return modules
+
+
+# Lets a user enter in a students details
+def do_add():
+    current_student = {}
+    current_student["name"] = input("Enter name: ")
+    current_student["modules"] = read_modules()
+
+    students.append(current_student)
+
+
+# Main Program
+users_choice = display_menu()
+while users_choice != "q":
+    if users_choice == "a":
+        do_add()
+    elif users_choice == "v":
+        do_view()
+    elif users_choice != "q":
+        print("\n\nPlease select either a, v or q\n\n")
+    users_choice = display_menu()
