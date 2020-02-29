0701e31caa14b0b27df47a42719b12ec3ac7c039
Daniel Mc Callion <dmceire@gmail.com>
2020-02-29 15:17:44 +0000

    Added second lab05 file for printing the summer months using tuples

---
+++ labs/Topic05-data_structures/lab5_02_summer_months.py
@@ -0,0 +1,20 @@
+# Daniel Mc Callion
+# This program prints the summer months
+
+months  = ("January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December")
+
+summer = months[4:7]
+
+for month in summer:
+    print(month)
