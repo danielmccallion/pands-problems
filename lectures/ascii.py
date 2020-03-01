63947f7b00a2a9ccb61438b74a75b3067f23feca
Daniel Mc Callion <dmceire@gmail.com>
2020-03-01 22:00:51 +0000

    Added lecture on files and ascii

---
+++ lectures/ascii.py
@@ -0,0 +1,5 @@
+# Daniel Mc Callion
+# Ascii Table
+
+for i in range(0, 256):
+    print(f"{i:3} {i:08b} {chr(i)}")
---
+++ lectures/read_a_file.py
@@ -0,0 +1,15 @@
+# Daniel Mc Callion
+
+
+with open(".gitignore", "r") as f:
+    for line in f:
+        print(line)
+
+#f = open(".gitignore", "r")
+
+#print(f.readline(), end="")
+
+#for line in f:
+ #   print(line, end="")
+
+#f.close()
