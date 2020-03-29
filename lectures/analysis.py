54ea7edd4decd7aea91e93b0e948f1209e0b3c09
Daniel Mc Callion <dmceire@gmail.com>
2020-03-29 21:03:34 +0100

    Added code and graphs from lecture on saving plots

---
+++ lectures/analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("http://www.biostat.jhsph.edu/~rpeng/useRbook/faithful.csv")
+
+plt.plot(df["eruptions"], df["waiting"], "b.")
+plt.title("eruptions vs waiting")
+plt.savefig("scatter.png")
+
+
+plt.clf()
+plt.hist(df["eruptions"])
+plt.savefig("eruptions.png")
+
+plt.clf()
+plt.hist(df["waiting"])
+plt.savefig("waiting.png")
